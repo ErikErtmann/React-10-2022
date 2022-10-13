e66abd6db9c52ff99a6a039e30bae4f6ccf4078a
ErikErtmann <115531590+ErikErtmann@users.noreply.github.com>
2022-10-13 17:32:05 +0300

    1. kodune

---
+++ Kodused 13.10/kodune/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <button className="nupp">Nupp</button>
+     <img className="pilt" src="https://uscar.ee/wp-content/uploads/2019/04/challenger-gt.png" alt="" />
+     <p>Väike test</p>
+     </div>
+
+  );
+}
+
+export default App;
