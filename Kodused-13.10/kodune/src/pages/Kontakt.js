a1534b89e0301d1055c2556de85e0194b2c02497
ErikErtmann <115531590+ErikErtmann@users.noreply.github.com>
2022-10-18 15:03:56 +0300

    18.10 laptop kodune

---
+++ Kodused-13.10/kodune/src/pages/Kontakt.js
@@ -0,0 +1,6 @@
+
+
+
+// const [aadress, määraAadress] = useState(&quot;Tallinn&quot;);
+// const [telefon, määraTelefon] = useState(&quot;5512345&quot;);
+// const [email, määraEmail] = useState(&quot;bla@baa.com&quot;);
