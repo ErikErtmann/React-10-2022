21c608d8fe5780cdda02a6ccc51eff95adbf6357
ErikErtmann <115531590+ErikErtmann@users.noreply.github.com>
2022-10-18 20:41:55 +0300

    Firebase, null, undefined

--- veebipood/src/Pages/Meist.js
+++ veebipood/src/Pages/Meist.js
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 function Meist() {
-    return (<div>Meist</div>);
+    // võtame vastu telefoni ja emaili
+    const [telefon, uuendaTelefoni] = useState(localStorage.getItem("telefon") || "Telefoninr pole lisatud");
+    const [n2itaEmaili, uuendaN2itaEmaili] = useState(false);
+
+return (
+<div>
+    <div>Meie telefon: 
+        {telefon}
+        { telefon.startsWith("+372") === false && telefon !== "Telefoninr pole lisatud" && <button onClick={() => uuendaTelefoni("+372" + telefon)}>Lisa suunakood</button>}
+    </div>
+    <div>Meie e-mail: 
+        { n2itaEmaili === true && localStorage.getItem("email")}
+        { n2itaEmaili === false && <button onClick={() => uuendaN2itaEmaili(true)}> Näita e-maili</button>}
+    </div>
+</div> );
 }
 
 export default Meist;
--- veebipood/src/Pages/Seaded.js
+++ veebipood/src/Pages/Seaded.js
@@ -1,5 +1,58 @@
+import { useRef, useState } from "react";
+
 function Seaded() {
-    return (<div>Muuda seadeid</div>);
+    // keele kuvamist (mis keeles sait on)
+    const [keel, uuendaKeel] = useState(localStorage.getItem("veebilehe_keel") || "SS" || null || undefined || "EST");
+    const telRef = useRef();
+    const emailRef = useRef();
+    // salvestame telefoninumbri ja emaili
+
+
+
+    const muudaKeelEST = () => {
+        uuendaKeel("EST");
+        localStorage.setItem("veebilehe_keel","EST");
+    }
+    const muudaKeelENG = () => {
+        uuendaKeel("ENG");
+        localStorage.setItem("veebilehe_keel","ENG");
+    }
+    const muudaKeelRUS = () => {
+        uuendaKeel("RUS");
+        localStorage.setItem("veebilehe_keel","RUS");
+    }
+
+
+
+    const sisestaTel = () => {
+        localStorage.setItem("telefon", telRef.current.value);
+    }
+    const sisestaEmail = () => {
+        localStorage.setItem("email", emailRef.current.value);
+    }
+
+    return (
+        <div>
+            <br /><br />
+            <label>Meie telefon</label>
+            <input ref={telRef} type="text" />
+            <button onClick={sisestaTel}>Sisesta</button>
+            <br />
+            
+            <label>Meie email</label>
+            <input ref={emailRef} type="text" />
+            <button onClick={sisestaEmail}>Sisesta</button>
+            <br />
+
+            <br /><br />
+            <div>Vali veebilehe keel:</div>
+            <button onClick={muudaKeelEST}>Eesti</button>
+            <button onClick={muudaKeelENG}>Inglise</button>
+            <button onClick={muudaKeelRUS}>Vene</button>
+            { keel === "EST" && <div>Veebilehe keel on eesti</div> }
+            { keel === "RUS" && <div>Veebilehe keel on vene</div> }
+            { keel === "ENG" && <div>Veebilehe keel on inglise</div> }
+        </div>);
 }
 
 export default Seaded;
